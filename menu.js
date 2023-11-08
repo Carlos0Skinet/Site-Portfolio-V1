b67c58eac74054664e71988bb741d56b887ec005
Carlos0Skinet <carlos.gabriel.business00@gmail.com>
2023-11-08 00:14:29 -0300

    Scroll reveal e href arrumados

--- menu.js
+++ menu.js
@@ -1,16 +1,24 @@
-
-let btnMenu = document.getElementById('btn-menu')
-let menu = document.getElementById('menu-mobile')
-let overlay = document.getElementById('overlay-menu')
+// Menu mobile
+let btnMenu = document.getElementById('btn-menu');
+let menu = document.getElementById('menu-mobile');
+let overlay = document.getElementById('overlay-menu');
 
 btnMenu.addEventListener('click', ()=>{
     menu.classList.add('abrir-menu')
-})
+});
 
 menu.addEventListener('click', ()=>{
     menu.classList.remove('abrir-menu')
-})
+});
 
 overlay.addEventListener('click', ()=>{
     menu.classList.remove('abrir-menu')
-})
+});
+// Fim menu mobile
+
+// Scroll reveal
+window.sr = ScrollReveal({ reset: true});
+
+sr.reveal('.portfolio', { 
+    duration: 1200});
+// Fim scroll reveal
